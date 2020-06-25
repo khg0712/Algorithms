1caba723e3db701f033e4b6986fe0b8e89f79210
김형규 <khg0712@midasit.com>
2020-06-25 16:02:01 +0900

    SNS 첫 실패 풀이

--- baekjoon(acmicpc)/SNS - 2533.js
+++ baekjoon(acmicpc)/SNS - 2533.js
@@ -3,8 +3,32 @@ function solution(input) {
 	const formattedInput = splittedInput.map((arr) =>
 		arr.map((e) => parseInt(e))
 	);
-	console.log(formattedInput);
 	const [nodeCnt] = formattedInput.shift();
+	const tree = createTree(nodeCnt, formattedInput);
+	console.log(bfs(tree, tree[1], true));
+}
+
+function createTree(nodeCnt, nodes) {
+	const tree = new Array(nodeCnt + 1).fill().map(() => []);
+	nodes.forEach(([parent, child]) => {
+		tree[parent].push(child);
+	});
+	return tree;
+}
+
+function bfs(tree, subTree, isParentEarly) {
+	if (subTree.length === 0) {
+		console.log(isParentEarly ? 0 : 1);
+		return isParentEarly ? 0 : 1;
+	}
+	if (isParentEarly)
+		return subTree.reduce(
+			(prev, cur) =>
+				prev +
+				Math.min(1 + bfs(tree, tree[cur], true), bfs(tree, tree[cur], false)),
+			0
+		);
+	return bfs(tree, subTree, true);
 }
 
 solution(`8
